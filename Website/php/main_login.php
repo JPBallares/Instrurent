f5c0725badb167206e9eef122b80ab4369bd12d8
Justine Ballares <ballaresjustine@gmail.com>
2018-05-02 14:06:18 +0800

    Added function to login

---
+++ Website/php/main_login.php
@@ -0,0 +1,94 @@
+<?php
+include 'access_db.php';
+
+$conn = OpenCon();
+session_start();
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
+
+$sql = "SELECT * FROM accounts where username = '$username';";
+$result = $conn->query($sql);
+if (isset($_POST['login'])) {
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            if ($row['password'] == $password) {
+
+                switch ($row['account_type']) {
+                    case "sa":
+                        $password = $_POST['password'];
+                        echo "
+                        <form method='POST' action='http://localhost:8084/Tenterent/sadmin_login' id='login_form'>
+                        <input type='text' name='username' value='$username'>
+                        <input type='password' name='password' value='$password'>
+                        <input type='submit' value='login' name='login'>
+                        </form>
+                        ";
+                        break;
+                    case "sp":
+                        $password = $_POST['password'];
+                        echo "
+                        <form method='POST' action='sp_login.php' id='login_form'>
+                            <input type='text' name='username' value='$username'>
+                            <input type='password' name='password' value='$password'>
+                            <input type='submit' value='login' name='login'>
+                        </form>
+                        ";
+                        break;
+                    case "c":
+                        $password = $_POST['password'];
+                        echo "
+                        <form method='POST' action='customer_login' id='login_form'>
+                            <input type='text' name='username' value='$username'>
+                            <input type='password' name='password' value='$password'>
+                            <input type='submit' value='login' name='login'>
+                        </form>
+                        ";
+                        break;
+                    case "a":
+                        $password = $_POST['password'];
+                        echo "
+                        <form method='POST' action='http://localhost:8084/Tenterent/admin_login' id='login_form'>
+                            <input type='text' name='username' value='$username'>
+                            <input type='password' name='password' value='$password'>
+                            <input type='submit' value='login' name='login'>
+                        </form>
+                        ";
+                        break;
+                }
+                echo "
+                <script>
+                window.onload = function(){
+                    document.getElementById('login_form').submit();
+                }
+                </script>
+                ";
+            } else {
+
+                echo "
+                    <script>
+                        alert('Wrong password.');
+                        window.history.back();
+                    </script>
+                ";
+
+            }
+        }
+    } else {
+        echo "
+            <script>
+                alert('Account doesn't exist.);
+                window.history.back();
+            </script>
+        ";
+    }
+
+} else {
+    echo "Failed";
+}
+
+CloseCon($conn);
---
+++ Website/php/sp_register.php
@@ -0,0 +1,113 @@
+<?php
+include 'access_db.php';
+$conn = OpenCon();
+
+$provider_name = $_POST['provider_name'];
+$address = $_POST['address'];
+$contact = $_POST['contact'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$account_type = 'sp';
+
+$provider_name = mysqli_real_escape_string($conn, $provider_name);
+$address = mysqli_real_escape_string($conn, $address);
+$contact = mysqli_real_escape_string($conn, $contact);
+$email = mysqli_real_escape_string($conn, $email);
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
+
+ if (empty($provider_name) || empty($address) || empty($email) || empty($username)
+     || empty($password) || empty($contact)) {
+     echo "
+         <script>
+             alert('You must fill up all neccessary fields.');
+             window.history.back();
+         </script>
+     ";
+     exit;
+ }
+
+ if (!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/", $user_email)) {
+     echo "
+         <script>
+             alert('Invalid Email');
+             window.history.back();
+         </script>
+     ";
+     exit;
+ }
+
+ if (!preg_match("^\+639[0-9]{9}$|^09[0-9]{9}$", $contact)) {
+     echo "
+         <script>
+             alert('Invalid Contact Number');
+             window.history.back();
+         </script>
+     ";
+     exit;
+ }
+
+ $sql = "SELECT * FROM accounts where user_email = '$email'";
+ $result = $conn->query($sql);
+
+ if ($result->num_rows > 0) {
+     echo "
+         <script>
+             alert('Email address that you have entered is already in use.');
+             window.history.back();
+         </script>
+     ";
+     exit;
+ }
+
+ $sql = "SELECT * FROM accounts where username = '$username'";
+ $result = $conn->query($sql);
+
+ if ($result->num_rows > 0) {
+     echo "
+         <script>
+             alert('Username that you have entered is already taken.');
+             window.history.back();
+         </script>
+     ";
+     exit;
+ }
+
+ if (strlen($password) < 8 || strlen($password) > 20) {
+     echo "
+         <script>
+             alert('Password length must be greater than 8 and less than 20 characters,');
+             window.history.back();
+         </script>
+     ";
+     exit;
+ }
+
+$password = sha1($password);
+
+$insert_stmt = "INSERT INTO `tenterent`.`accounts` (`email`, `username`, `password`, `account_type`) VALUES ('$email', '$username', '$password', '$account_type');";
+
+$sql = "SELECT * FROM accounts ORDER BY account_id DESC LIMIT 1;";
+
+if ($conn->query($insert_stmt) === true) {
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+        $accountid = $row['account_id'];
+    }
+    $insert_stmt = "INSERT INTO `tenterent`.`service_provider` (`provider_name`, `provider_contact`, `provider_address`, `accountid`) VALUES ('$provider_name', '$contact', '$address', '$accountid');";
+    if ($conn->query($insert_stmt) === true) {
+        echo "
+        <script>
+            alert('You are now registered.');
+            window.location.replace('/');
+        </script>
+    ";
+    } else {
+        echo "Error: " . $query . "<br>" . $conn->error;
+    }
+} else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+}
+
+CloseCon($conn);
