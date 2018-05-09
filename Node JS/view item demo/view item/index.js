a3c211623c4e8a9c266cc9109c71edc3b2790bfc
JRSibaen <31525596+JRSibaen@users.noreply.github.com>
2018-05-09 20:12:38 +0800

    demo for view item for customers

---
+++ Node JS/view item demo/view item/index.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mysql = require('mysql'),
+    sha1 = require('sha1');
+
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3308,
+    user: 'root',
+    password: '',
+    database: 'tenterent'
+    
+});
+ 
+// Running Server Details.
+var server = app.listen(8082, function () {
+    'use strict';
+    var host = server.address().address,
+        port = server.address().port;
+    console.log("Example app listening at %s:%s Port", host, port);
+});
+
+app.post('/item', urlencodedParser, function (req, res) {
+    'use strict';
+    var sp = "'" + req.body.sp + "'",
+        item = "'" + req.body.product_name + "'",
+        html = '',
+        sql = 'SELECT item_name, price, renting_fee, stock, type_name, provider_name, provider_contact, provider_address FROM items natural join item_type natural join service_provider where item_id = ' + item;
+    connection.query(sql, function (err, result, field) {
+        if (err) {
+            throw err;
+        }
+        
+        Object.keys(result).forEach(function (key) {
+            var row = result[key];
+            html += row.item_name + "<br>" + row.price + "<br>" + row.renting_fee + "<br> " + row.stock + "<br> " + row.type_name + "<br>" + row.provider_name + "<br>" + row.provider_contact + "<br>" + row.provider_address;
+            res.send(html);
+        });
+    });
+});
