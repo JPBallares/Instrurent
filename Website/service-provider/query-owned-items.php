94e3c0937c14dddf28fdfb8f45e209a9d059811e
cs-13 <31474500+cs-13@users.noreply.github.com>
2018-05-12 19:47:51 +0800

    Update query-owned-items.php
    Changed the source code for better compatibility with the changed UI.

--- Website/service-provider/query-owned-items.php
+++ Website/service-provider/query-owned-items.php
@@ -6,28 +6,16 @@
 
 	if($res->num_rows > 0) {
 		while($row = $res->fetch_assoc()) {
-//			echo "name: " . $row["item_name"] . "<br>price: " . $row["price"] . "<br>renting fee: " . $row["renting_fee"] . "stock: " . $row["stock"];
-//            echo '<img src="data:image;base64,' . base64_encode($row["item_image"]) . '"/>';
-            
-            if($counter == 0) {
-                echo "<div class='row cus-row'>";
-            }
-            
-            if($counter++ == 4) {
-                echo '<div class="col-sm cus-col-sm">';
-                echo '    <img class="sp-item-image" src="data:image;base64,' . base64_encode($row["item_image"]) . '"/>';
-				echo '    <div class="sp-item-info">';
-				echo '    <p>Name: </p>';
-				echo '        <p>Price:'. $row["price"] . '</p>';
-				echo '        <p>Renting Fee:' . $row["renting_fee"] . '</p>';
-				echo '        <p>Stock:' . $row["stock"] . '</p>';
-                echo '        </div>';
-                echo '</div>';
-            }
+            echo '<div class="card cus-card" style="width: 300px; height: 350px;">';
+            echo '<img style="width: 300px; height: 150px;" src="data:image;base64,' . base64_encode($row["item_image"]) . '" alt="Card image cap">';
+            echo '    <div class="card-body">';
+            echo '        <h5 class="card-title">Name</h5>';
+            echo '        <p class="card-text">Price: </p>';
+            echo '        <p class="card-text">Renting Fee: </p>';
+            echo '        <p class="card-text">Stock: </p>';
+            echo '    </div>';
+            echo '</div>';
 		}
 	}
-
-    echo "</div>";
-
 	$con_db->close();
 ?>
