ec3e024e7524f97b29deaec9e91c2ad675c3662e
Marc Lawrence <gregoriorenz12@gmail.com>
2018-05-15 21:51:41 +0800

    up

--- Node JS/view item/index.js
+++ Node JS/view item/index.js
@@ -24,24 +24,36 @@ var server = app.listen(8082, function () {
 
 app.post('/item', urlencodedParser, function (req, res) {
     'use strict';
-    var item_id = "'" + req.body.id + "'",
-        html = '',
-        sql = 'SELECT item_name, price, renting_fee, stock, type_name, provider_name, provider_contact, provider_address, item_image FROM items natural join item_type natural join service_provider where item_id = ' + item_id;
-    connection.query(sql, function (err, result, field) {
-        if (err) {
-            throw err;
-        }
-        
-        Object.keys(result).forEach(function (key) {
-			var row = result[key];
-			var blob = new Blob()
-            var buffer = new Buffer(row.item_image,'binary');
-			var image = buffer.toString('base64');
-			html += "<body>";
-			html += row.item_name + "<br>" + row.price + "<br>" + row.renting_fee + "<br> " + row.stock + "<br> " + row.type_name + "<br>" + row.provider_name + "<br>" + row.provider_contact + "<br>" + row.provider_address + " <br>";
-			html += "<img src={'data:image/;'" + image +"}";
-			html += "</body>";
-            res.send(html);
-        });
-    });
+    var html = '';
+	var counter = 'SELECT COUNT(item_id) as count FROM items NATURAL JOIN item_type NATURAL JOIN service_provider';
+	
+	connection.query(counter, function(err, result, field){
+			if(err){
+				throw err;
+			}
+			for (var x = 1; x <= result[0].count; x++){
+				var sql = 'SELECT * FROM items NATURAL JOIN item_type NATURAL JOIN service_provider where item_id = ' + x;
+				connection.query(sql, function (err, result, field) {
+				if (err) {
+					throw err;
+				}
+
+				Object.keys(result).forEach(function (key) {
+					var row = result[key];
+					var buffer = new Buffer(row.item_image,'binary');
+					var image = buffer.toString('base64');
+					html += "<body>";
+					html += row.item_name + "<br>" + row.price + "<br>" + row.renting_fee + "<br> " + row.stock + "<br> " + row.type_name + "<br>" + row.provider_name + "<br>" + row.provider_contact + "<br>" + row.provider_address + " <br>";
+					html += `<img src="data:image;base64,` + image +`">`;
+					html += "</body>";
+					res.send(html);
+					res.run;
+
+				});
+			});
+			}
+
+		
+			
+	});	
 });
