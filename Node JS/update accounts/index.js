059fc4728163d888a83f55698ff85783731d519a
KKK <31525597+KuKluxK@users.noreply.github.com>
2018-05-17 06:07:44 +0800

    Update index.js

--- Node JS/update accounts/index.js
+++ Node JS/update accounts/index.js
@@ -22,7 +22,7 @@ var server = app.listen(8091, function () {
 
 var db = mysql.createConnection({
     host: 'localhost',
-    port: 3308,
+    port: 3306,
     user: 'root',
     password: '',
     database: 'tenterent'
