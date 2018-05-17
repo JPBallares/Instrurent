2fd881710731979fa067fbf7c21354a3c36f86e7
Justine Ballares <ballaresjustine@gmail.com>
2018-05-17 10:58:08 +0800

    fix on navigatinos

--- Website/service-provider/edit_profile.php
+++ Website/service-provider/edit_profile.php
@@ -36,7 +36,7 @@
 
     <body>
         <nav class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img src="./css/img/Logo.png" class="logo_s">TentERent</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
--- Website/service-provider/view-accepted.php
+++ Website/service-provider/view-accepted.php
@@ -34,7 +34,7 @@
     
 <body>
     <nav class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg">
-    <a class="navbar-brand" href="#"><img src="./css/img/Logo.png" class="logo_s">TentERent</a>
+    <a class="navbar-brand" href="/"><img src="./css/img/Logo.png" class="logo_s">TentERent</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
